2f24c36f905c69d2d026db96e6a56fdf0bde1391
Joshua Grosser <joshua.grosser@gmail.com>
2020-02-28 13:22:56 -0500

    Updated the RecipeItemComponent and associated classes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  items: Recipe[] = [];
+
+  constructor() {
+    const recipe001 = new Recipe('Recipe 001', 'lorem ipsum sit dolor', '/images');
+    const recipe002 = new Recipe('Recipe 002', 'lorem ipsum sit dolor', '/images');
+    const recipe003 = new Recipe('Recipe 003', 'lorem ipsum sit dolor', '/images');
+    this.items.push(recipe001);
+    this.items.push(recipe002);
+    this.items.push(recipe003);
+  }
 
   ngOnInit() {
   }
